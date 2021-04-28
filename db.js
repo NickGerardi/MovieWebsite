59c31617e85129042699b0cc4efb9e3a517c1f91
NickGerardi <npg5220@psu.edu>
2021-04-28 15:52:42 -0400

    Comments in db.js

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 //copied from zybooks 11.8.7
 
 // db.js
+//WARNING
+//I could not get npm install mongoose --save to run with this application, so at this point I can not get it to run npm start either
+//I tried adding the dependency by hand in package.json, and it worked, but only temporarily
 const mongoose = require("mongoose");
 mongoose.set("useUnifiedTopology", true);
 //mongoose.connect("mongodb://localhost/mydb",
