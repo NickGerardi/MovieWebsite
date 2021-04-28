15aacaa09aa461dccb76880dc2a436282d356e2f
NickGerardi <npg5220@psu.edu>
2021-04-28 15:23:50 -0400

    Final Mongo Commit(hopefully)

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const Movie = require("./models/movie");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -18,6 +20,25 @@ app.use(express.static(path.join(__dirname, 'public'),{extensions: 'html'}));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.post("/create", function(req, res) {
+
+    // Create a student from the submitted form data
+    const mov = new Movie({
+       name: req.body.name,
+       rating: req.body.rating,
+       releaseDate: new Date(req.body.releasedate)
+    });
+ 
+    mov.save(function(err, mov) {
+       if (err) {
+          res.status(400).send(err);
+       } 
+       else {
+          res.send("Movie was saved.");
+       }
+    });
+ });
+ 
 module.exports = app;
 
 
